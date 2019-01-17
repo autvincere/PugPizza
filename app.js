56fedc92c89671d4cd40eab57e1a3b7092443a30
Eduardo Palma <eduardo.palma@yr.com>
2019-01-17 17:56:10 -0300

    se agrega funciones replicadoras de mensaje

--- app.js
+++ app.js
@@ -1,27 +1,72 @@
-'use strict'
+'use strict';
 
-const 
-express = require('express'),
-bodyParser = require('body-parser'),
-request = require('request');
+const express = require('express');
+const bodyParser = require('body-parser');
+const request = require('request');
+const access_token = "EAAEwCIsMWcwBAAK0bL7EdGWPYkRXP4VmEun809Jp0EdtURqwJRPLI8OJbUln0Yr70ZB6QW7oeoeqEwHXzU4meO5rbcyzTmKE7uyecs8A84CzaRQbqn168OMDknevxHl0jaDumARRMdXcqzYZARLSxitrGKKZCg5ZCKBjsoZBpZAgZDZD"
 
 const app = express();
 
-app.set('port',5000);
+app.set('port', 4000);
 app.use(bodyParser.json());
 
-app.get('/',function(req,response){
-     response.send('hola mundo!');
+app.get('/', function(req, response){
+    response.send('Hola Mundo!');
 })
 
-app.get('/webhook',function(req,response){
-     if (req.query['hub.verify_token']=== 'pugpizza_token') {
-          response.send(req.query['hub.challenge']);
-     } else {
-          response.send('Pug Pizza no tienes permisos.')
-     }
-})
+app.get('/webhook', function(req, response){
+    if(req.query['hub.verify_token'] === 'pugpizza_token'){
+        response.send(req.query['hub.challenge']);
+    } else {
+        response.send('Pug Pizza no tienes permisos.');
+    }
+});
 
-app.listen(app.get('port'),function(){
-     console.log('Nuestro servidor esta funcionando en el puerto', app.get('port'));
-})
+app.post('/webhook/', function(req, res){
+    const webhook_event = req.body.entry[0];
+    if(webhook_event.messaging) {
+        webhook_event.messaging.forEach(event => {
+            handleMessage(event);
+            //console.log(event);
+            
+        });
+    }
+    res.sendStatus(200);
+});
+
+function handleMessage(event){
+    const senderId = event.sender.id;
+    const messageText = event.message.text;
+    const messageData = {
+        recipient: {
+            id: senderId
+        },
+        message: {
+            text: messageText
+        }
+    }
+    callSendApi(messageData);
+}
+
+function callSendApi(response) {
+    request({
+        "uri": "https://graph.facebook.com/v2.6/me/messages",
+        "qs": {
+            "access_token": access_token
+        },
+        "method": "POST",
+        "json": response
+    },
+    function(err) {
+        if(err) {
+            console.log('Ha ocurrido un error')
+        } else {
+            console.log('Mensaje enviado')
+        }
+    }
+)
+}
+
+app.listen(app.get('port'), function(){
+    console.log('Nuestro servidor esta funcionando con el barto en el puerto: ', app.get('port'));
+});
